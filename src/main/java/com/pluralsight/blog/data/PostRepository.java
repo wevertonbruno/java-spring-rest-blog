7a3a0596d051816567d5a9d3921579c9e3b3c5f1
Weverton Bruno <oieusouweverton@hotmail.com>
2021-08-24 16:12:47 -0300

    feat: Add fake data and find by title method

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,5 +4,11 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 @RestResource
-public interface PostRepository extends JpaRepository<Post, Long>{}
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+    @RestResource(rel = "contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
+}
